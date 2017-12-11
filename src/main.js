2dc110d2d55aeca21f63f05defc7b10001727e5a
alien <13373935140@163.com>
2017-12-11 11:54:22 +0800

    mock

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import * as filters from './filters' // 全局filter
 import './icons' // icon
 import './errorLog'// error log
 import './permission' // 权限
-import './mock' // 该项目所有请求使用mockjs模拟
+import '../mock' // 该项目所有请求使用mockjs模拟
 
 Vue.use(Element, {
   i18n: (key, value) => i18n.t(key, value)
